32b315b1e379b27f0cd21554baa8f30e96be26fb
UrsuGheorghe <gursu@ms3-inc.com>
2020-10-31 13:34:33 +0200

    Added Rolles and Create User for read, and Auth methods for delete create

--- src/main/java/com/gursu/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/gursu/springsecuritydemo/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.gursu.springsecuritydemo.config;
 
+import com.gursu.springsecuritydemo.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,7 +19,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean
@@ -25,10 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
                 User.builder()
-                        .username("admin")
-                        .password(encoder().encode("admin"))
-                        .roles("ADMIN")
-                        .build());
+                        .password("admin")
+                        .username(encoder().encode("admin"))
+                        .roles(Role.ADMIN.name())
+                        .build(),
+
+                User.builder()
+                        .username("user")
+                        .password(encoder().encode("user"))
+                        .roles(Role.USER.name())
+                        .build()
+        );
     }
 
     @Bean
--- src/main/java/com/gursu/springsecuritydemo/model/Developer.java
+++ src/main/java/com/gursu/springsecuritydemo/model/Developer.java
@@ -9,4 +9,6 @@ public class Developer {
     private Long id;
     private String firstName;
     private String lastName;
+
+
 }
